caa0913ada992043950f154ade3ced07af0dc5c1
bushoki <239450036+bushoki@users.noreply.github.com>
2026-01-28 08:26:55 -0500

    Fix: Change auth flow from pkce to implicit to resolve 'operation was aborted' error

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -14,7 +14,7 @@ export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: true,
-    flowType: 'pkce',
+    flowType: 'implicit',
   },
   global: {
     headers: {
